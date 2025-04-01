a2b0214467d5fb3f8594e16db9696c4de0e7144b
ramsesar1 <90067815+ramsesar1@users.noreply.github.com>
2025-03-31 22:34:23 -0700

    se arregla que el draw estuviera refrescando las imagenes y parpadearan

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let cards = [];
 let flippedCards = [];
 let matchedPairs = 0;
 let canFlip = true;
+let loadedImages = {}; 
 
 function preload() {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
@@ -11,9 +12,29 @@ function preload() {
       let promises = data.results.map(pokemon => fetch(pokemon.url).then(res => res.json()));
       Promise.all(promises).then(pokemons => {
         pokemonImages = pokemons.map(p => p.sprites.front_default);
-        setupGame();
+        let imagePromises = pokemonImages.map(url => {
+          return new Promise((resolve) => {
+            loadImage(url, img => {
+              loadedImages[url] = img;
+              resolve();
+            });
+          });
+        });
+        
+        Promise.all(imagePromises).then(() => {
+          setupGame();
+        });
       });
     });
+}
+
+function setup() {
+  createCanvas(400, 400);
+  if (pokemonImages.length === 0) {
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("Cargando Pokémons...", width/2, height/2);
+  }
 }
 
 function setupGame() {
@@ -27,38 +48,53 @@ function setupGame() {
       matched: false
     });
   }
-  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+  
+  if (cards.length === 0) {
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text("Cargando Pokémons...", width/2, height/2);
+    return;
+  }
+  
   for (let i = 0; i < cards.length; i++) {
     let x = (i % 5) * 80;
     let y = floor(i / 5) * 100;
+    
     if (cards[i].flipped || cards[i].matched) {
-      loadImage(cards[i].img, img => image(img, x, y, 80, 100));
+      if (loadedImages[cards[i].img]) {
+        image(loadedImages[cards[i].img], x, y, 80, 100);
+      }
     } else {
       fill(100);
       rect(x, y, 80, 100);
+      
+      fill(50);
+      textSize(20);
+      textAlign(CENTER, CENTER);
+      text("?", x + 40, y + 50);
     }
   }
 }
 
 function mousePressed() {
-  if (!canFlip) return;
+  if (!canFlip || cards.length === 0) return;
   
   let col = floor(mouseX / 80);
   let row = floor(mouseY / 100);
   let index = row * 5 + col;
   
-  if (index < cards.length && !cards[index].flipped && !cards[index].matched) {
+  if (index >= 0 && index < cards.length && !cards[index].flipped && !cards[index].matched) {
     cards[index].flipped = true;
     flippedCards.push(index);
-  }
-  
-  if (flippedCards.length === 2) {
-    canFlip = false;
-    setTimeout(checkMatch, 1000);
+    
+    if (flippedCards.length === 2) {
+      canFlip = false;
+      setTimeout(checkMatch, 1000);
+    }
   }
 }
 
@@ -76,14 +112,15 @@ function checkMatch() {
   canFlip = true;
   
   if (matchedPairs === pokemonImages.length) {
-    alert("awebo");
+    alert("¡Lo lograste!");
   }
 }
 
 function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
+  let newArray = [...array]; 
+  for (let i = newArray.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
   }
-  return array;
+  return newArray;
 }
